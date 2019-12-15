9a73916ad5a41b2a8ee5807b167e26da851896d5
Alexander Senier <senier@componolit.com>
2019-12-15 22:10:08 +0100

    Support 'Valid attribute

--- tests/test_fsm_parse.py
+++ tests/test_fsm_parse.py
@@ -1,5 +1,7 @@
 import unittest
 
+from rflx.expression import Equal, NotEqual, Variable
+from rflx.fsm_expression import Valid
 from rflx.fsm_parser import FSMParser
 
 
@@ -7,6 +9,14 @@ class TestFSM(unittest.TestCase):
     def setUp(self) -> None:
         self.maxDiff = None  # pylint: disable=invalid-name
 
-    @classmethod
-    def test_simple_equation(cls) -> None:
-        FSMParser.condition().parseString("Foo.Bar = abc")
+    def test_simple_equation(self) -> None:
+        result = FSMParser.condition().parseString("Foo.Bar = abc")[0]
+        self.assertEqual(result, Equal(Variable("Foo.Bar"), Variable("abc")))
+
+    def test_simple_inequation(self) -> None:
+        result = FSMParser.condition().parseString("Foo.Bar /= abc")[0]
+        self.assertEqual(result, NotEqual(Variable("Foo.Bar"), Variable("abc")))
+
+    def test_valid(self) -> None:
+        result = FSMParser.condition().parseString("Something'Valid")[0]
+        self.assertEqual(result, Valid(Variable("Something")))
