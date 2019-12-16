8ec151bcab2da4be39564780358eb65f10ab8798
Alexander Senier <senier@componolit.com>
2019-12-16 08:07:08 +0100

    Implement conjunction and disjunction

--- tests/test_fsm_parse.py
+++ tests/test_fsm_parse.py
@@ -1,6 +1,6 @@
 import unittest
 
-from rflx.expression import Equal, NotEqual, Variable
+from rflx.expression import And, Equal, NotEqual, Or, Variable
 from rflx.fsm_expression import Valid
 from rflx.fsm_parser import FSMParser
 
@@ -20,3 +20,23 @@ class TestFSM(unittest.TestCase):
     def test_valid(self) -> None:
         result = FSMParser.condition().parseString("Something'Valid")[0]
         self.assertEqual(result, Valid(Variable("Something")))
+
+    def test_conjunction_valid(self) -> None:
+        result = FSMParser.condition().parseString("Foo'Valid and Bar'Valid")[0]
+        self.assertEqual(result, And(Valid(Variable("Foo")), Valid(Variable("Bar"))))
+
+    def test_conjunction(self) -> None:
+        result = FSMParser.condition().parseString("Foo = Bar and Bar /= Baz")[0]
+        self.assertEqual(
+            result,
+            And(
+                Equal(Variable("Foo"), Variable("Bar")), NotEqual(Variable("Bar"), Variable("Baz"))
+            ),
+        )
+
+    def test_disjunction(self) -> None:
+        result = FSMParser.condition().parseString("Foo = Bar or Bar /= Baz")[0]
+        self.assertEqual(
+            result,
+            Or(Equal(Variable("Foo"), Variable("Bar")), NotEqual(Variable("Bar"), Variable("Baz"))),
+        )
