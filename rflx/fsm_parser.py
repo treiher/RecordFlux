74367b31f5b7647e53758661c94db5d97e7d424c
Alexander Senier <senier@componolit.com>
2019-12-16 08:07:08 +0100

    Implement conjunction and disjunction

--- rflx/fsm_parser.py
+++ rflx/fsm_parser.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from pyparsing import Keyword, Literal, StringEnd, Token, infixNotation, opAssoc
+from pyparsing import Forward, Keyword, Literal, StringEnd, Token, infixNotation, opAssoc
 
-from rflx.expression import FALSE, TRUE, Equal, Expr, NotEqual, Variable
+from rflx.expression import FALSE, TRUE, And, Equal, Expr, NotEqual, Or, Variable
 from rflx.fsm_expression import Valid
 from rflx.parser import Parser
 
@@ -19,6 +19,16 @@ class FSMParser:
         return NotEqual(t[0], t[2])
 
     @classmethod
+    def __parse_conjunction(cls, tokens: List[List[Expr]]) -> Expr:
+        t = tokens[0]
+        return And(t[0], t[2])
+
+    @classmethod
+    def __parse_disjunction(cls, tokens: List[List[Expr]]) -> Expr:
+        t = tokens[0]
+        return Or(t[0], t[2])
+
+    @classmethod
     def expression(cls) -> Token:
         literal = Parser.boolean_literal()
         literal.setParseAction(lambda t: TRUE if t[0] == "True" else FALSE)
@@ -29,17 +39,20 @@ class FSMParser:
         valid = identifier() + Literal("'") - Keyword("Valid")
         valid.setParseAction(lambda t: Valid(t[0]))
 
-        simple_expression = literal | valid | identifier
+        expr = Forward()
+        simple_expr = literal | valid | identifier | expr
 
-        equation = infixNotation(
-            simple_expression,
+        expr <<= infixNotation(
+            simple_expr,
             [
                 (Keyword("="), 2, opAssoc.LEFT, cls.__parse_equation),
                 (Keyword("/="), 2, opAssoc.LEFT, cls.__parse_inequation),
+                (Keyword("and"), 2, opAssoc.LEFT, cls.__parse_conjunction),
+                (Keyword("or"), 2, opAssoc.LEFT, cls.__parse_disjunction),
             ],
         )
 
-        return equation | simple_expression
+        return expr
 
     @classmethod
     def condition(cls) -> Token:
